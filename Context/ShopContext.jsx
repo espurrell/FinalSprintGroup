0e048dff4d4ee9e6c5bb7689eeaa0a9090b3154b
espurrell <150044207+espurrell@users.noreply.github.com>
2024-04-14 16:37:38 -0230

    Add files via upload

---
+++ Context/ShopContext.jsx
@@ -0,0 +1,97 @@
+import React, { createContext, useState } from "react";
+import all_product from "../Components/Assets/all_product";
+
+export const ShopContext = createContext(null);
+
+const getDefaultCart = () => {
+  let cart = {};
+  for (let index = 0; index < all_product.length + 1; index++) {
+    cart[index] = 0;
+  }
+  return cart;
+};
+
+const ShopContextProvider = (props) => {
+  const [cartItems, setCartItems] = useState(getDefaultCart());
+
+  const addToCart = (itemId) => {
+    setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }));
+    console.log(cartItems);
+  };
+
+  const removeFromCart = (itemId) => {
+    if (cartItems[itemId] > 0) {
+      setCartItems((prev) => ({
+        ...prev,
+        [itemId]: prev[itemId] - 1, // Decrease the quantity by one
+      }));
+    }
+  };
+
+  const getTotalCartAmount = () => {
+    let totalAmount = 0;
+    for (const item in cartItems) {
+      if (cartItems[item] > 0) {
+        let itemInfo = all_product.find(
+          (product) => product.id === Number(item)
+        );
+        totalAmount += cartItems[item] * itemInfo.new_price;
+      }
+    }
+    return totalAmount.toFixed(2);
+  };
+
+  const getTotalCartAmountTax = () => {
+    let totalAmount = 0;
+    for (const item in cartItems) {
+      if (cartItems[item] > 0) {
+        let itemInfo = all_product.find(
+          (product) => product.id === Number(item)
+        );
+        totalAmount += cartItems[item] * itemInfo.new_price;
+      }
+    }
+    return (totalAmount * 0.15).toFixed(2);
+  };
+
+  const getTotalCartAmountTaxed = () => {
+    let totalAmount = 0;
+    for (const item in cartItems) {
+      if (cartItems[item] > 0) {
+        let itemInfo = all_product.find(
+          (product) => product.id === Number(item)
+        );
+        totalAmount += cartItems[item] * itemInfo.new_price;
+      }
+    }
+    return Math.round(totalAmount * 1.15 * 100) / 100;
+  };
+
+  const getTotalCartItems = () => {
+    let totalItem = 0;
+    for (const item in cartItems) {
+      if (cartItems[item] > 0) {
+        totalItem += cartItems[item];
+      }
+    }
+    return totalItem;
+  };
+
+  const contextValue = {
+    getTotalCartItems,
+    getTotalCartAmount,
+    getTotalCartAmountTax,
+    getTotalCartAmountTaxed,
+    all_product,
+    cartItems,
+    addToCart,
+    removeFromCart,
+  };
+  return (
+    <ShopContext.Provider value={contextValue}>
+      {props.children}
+    </ShopContext.Provider>
+  );
+};
+
+export default ShopContextProvider;
